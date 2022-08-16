602583c6711674708386c68949645b5431ed2cde
Lucas Hissinger <91745215+LucasHissinger@users.noreply.github.com>
2022-08-16 09:20:00 +0200

    Add error function and update links

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ for (item of buttons) {
     item.addEventListener('click', (e) => {
         button_text = e.target.innerText
         console.log("button : " + button_text);
-        if (screen_value.innerText == 0) {
+        if (screen_value.innerText == 0 && button_text != 'C' && button_text != '=') {
             expression = button_text;
         } else {
             if (button_text == 'C') {
@@ -26,12 +26,17 @@ for (item of buttons) {
             }
         }
         screen_value.innerText = expression;
+        if (expression.length > 34) {
+            alert('too many characters for me to display :(')
+            expression = '0';
+            screen_value.innerText = expression;
+        }
     });
 }
 
 window.onerror = function(){
     alert("PLEASE INPUT VALID EXPRESSION");
-    screenValue = "";
-    screen.value = screenValue;
+    expression = '0';
+    screen_value.innerText = expression;
     console.clear();
 }
